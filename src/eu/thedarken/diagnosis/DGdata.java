4e158aa5d345cd98b96c9e0a68481ae841179e6a
darken <d4rken@gmail.com>
2012-04-29 19:13:47 +0200

    Fixed data not being pulled for new core info only

--- src/eu/thedarken/diagnosis/DGdata.java
+++ src/eu/thedarken/diagnosis/DGdata.java
@@ -639,7 +639,7 @@ public class DGdata {
 	        				
 	        			} else if(x==1||x==2||x==3){
 	        				doapps = true;
-	        			} else if(x==4||x==5||x==6||x==7||x==8||x==9) {
+	        			} else if(x==4||x==5||x==6||x==7||x==8||x==9|| (x>=44 && x <=67)) {
 	        				docpu = true;
 	        			} else if(x==10||x==11||x==12||x==13||x==14) {
 	        				domem = true;
@@ -654,7 +654,7 @@ public class DGdata {
 	        				//mContext.unregisterReceiver(batteryReceiver);
 	        			} else if(x==25||x==26) {
 	        				dodisk = true;
-	        			} else if(x==27) {
+	        			} else if(x==27||x==40||x==41||x==42||x==43) {
 	        				dofreq = true;
 	        			} else if(x==28||x==29||x==30||x==31) {
 	        				dospace = true;
